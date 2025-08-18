7a5c680f511acaec1dc5a31f8bf0119654b12b54
Lucas-Maurice Stein <lms@koempf24.de>
2025-08-18 10:12:50 +0200

    adjusted json and vite for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// 🔁 Hier den Repo-Namen eintragen
 export default defineConfig({
     plugins: [react()],
-    base: '/freshprinceofberlin/', // z. B. dein GitHub-Repo-Name
 });
